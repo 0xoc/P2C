9b895a86bcd48dcb483a4fc0e825e79fa6f7e2df
Ali Parvizi <snparvizi75@gmail.com>
2022-01-02 06:26:32 -0800

    detect id and number tokens and properly identify reserved keywords

--- lexer.py
+++ lexer.py
@@ -2,6 +2,9 @@ import ply.lex as lex
 
 
 class P2CLexer(object):
+    def __init__(self):
+        self.lexer = None
+
     # keyword constants
     IF = 'if'
     OR = 'or'
@@ -41,7 +44,6 @@ class P2CLexer(object):
         OR: LOGIC,
         NOT: LOGIC,
     }
-
     relop_symbols = {
         '==': '==',
         '!=': '!=',
@@ -50,7 +52,6 @@ class P2CLexer(object):
         '>': '>',
         '>=': '>='
     }
-
     logical_symbols = {
         'and': '&&',
         'or': '||',
@@ -60,7 +61,6 @@ class P2CLexer(object):
         '||': '||',
         '!': '!',
     }
-
     operator_symbols = {
         # arithmetic
         '+': '+',
@@ -72,7 +72,7 @@ class P2CLexer(object):
     }
 
     # all the possible tokens
-    tokens = [ID, NUMBER, LOGIC, RELOP, OPERATOR, LPRAN, RPRAN, LBRACE, RBRACE, SEP, ]
+    tokens = [ID, NUMBER, LOGIC, RELOP, OPERATOR, LPRAN, RPRAN, LBRACE, RBRACE, SEP]
     tokens += list(reserved.values())
     # remove duplicate values
     tokens = list(set(tokens))
@@ -86,36 +86,56 @@ class P2CLexer(object):
 
     def t_ID(self, t):
         r"""[a-zA-Z_][a-zA-Z_0-9]*"""
+        t.type = self.reserved.get(t.value, self.ID)
+        if t.type == self.LOGIC:
+            t.value = self.logical_symbols.get(t.value)
 
-        # detect reserved word
-        if t.value in self.reserved.keys():
-            # detect logical reserved word
-            _token_mapped_value = self.reserved.get(t.value)
-            if _token_mapped_value == 'LOGIC':
-                t.type = 'LOGIC',
-                t.value = self.logical_symbols.get(t.value)
-
-            # not logical reserved word
-            else:
-                t.type = t.value
-                t.value = _token_mapped_value
         return t
 
     def t_NUMBER(self, t):
         r"""\d+\.?\d+"""
-        t.type = 'NUMBER'
-
-    # Define a rule so we can track line numbers
-    def t_newline(self, t):
-        r"""\n+"""
-        t.lexer.lineno += len(t.value)
+        t.type = self.NUMBER
+        t.value = float(t.value)
+        return t
 
     # A string containing ignored characters (spaces and tabs)
     t_ignore = ' \t'
 
-    def print_tokens(self):
-        print(self.tokens)
+    # Define a rule so we can track line numbers
+    @staticmethod
+    def t_newline(t):
+        r"""\n+"""
+        t.lexer.lineno += len(t.value)
+        # Error handling rule
+
+    @staticmethod
+    def t_error(t):
+        print("Illegal character '%s'" % t.value[0])
+
+    # Builds the lexer
+    def build(self, **kwargs):
+        self.lexer = lex.lex(module=self, **kwargs)
+
+    # Test it output
+    def test(self, data):
+        self.lexer.input(data)
+        while True:
+            tok = self.lexer.token()
+            if not tok:
+                break
+            print(tok)
 
 
 lexer = P2CLexer()
-lexer.print_tokens()
+lexer.build()  # Build the lexer
+lexer.test("""
+sina
+87
+23423423.435
+ali parvizi
+for not
+and
+or ||
+""")  # Test it
+
+
