8f6aa02020b33a8d4d6bbf378c957b189b87ce77
Ali Parvizi <snparvizi75@gmail.com>
2022-01-02 06:04:21 -0800

    use constant variables for string literals

--- lexer.py
+++ lexer.py
@@ -2,57 +2,80 @@ import ply.lex as lex
 
 
 class P2CLexer(object):
+    # keyword constants
+    IF = 'if'
+    OR = 'or'
+    AND = 'and'
+    ELSE = 'else'
+    FOR = 'for'
+    WHILE = 'while'
+    RANGE = 'range'
+    IN = 'in'
+    NOT = 'not'
+    BREAK = 'break'
+    CONTINUE = 'continue'
+
+    # token constants
+    NUMBER = 'NUMBER'
+    ID = 'ID'
+    LOGIC = 'LOGIC'
+    RELOP = 'RELOP'
+    OPERATOR = 'OPERATOR'
+    LPRAN = 'LPRAN'
+    RPRAN = 'RPRAN'
+    LBRACE = 'LBRACE'
+    RBRACE = 'RBRACE'
+    SEP = 'SEP'
+
     # reserved words
     reserved = {
-        'and': 'AND',
-        'or': 'OR',
-        'if': 'IF',
-        'for': 'FOR',
-        'while': 'WHILE',
-        'in': 'IN',
-        'range': 'RANGE',
-        'break': 'BREAK',
-        'continue': 'CONTINUE',
+        IF: IF,
+        FOR: FOR,
+        WHILE: WHILE,
+        IN: IN,
+        RANGE: RANGE,
+        BREAK: BREAK,
+        CONTINUE: CONTINUE,
+        # some logical symbols are also reserved words
+        AND: LOGIC,
+        OR: LOGIC,
+        NOT: LOGIC,
     }
 
-    comparison_symbols = [
-        'EQ',
-        'NEQ',
-        'LT',
-        'LTE',
-        'GT',
-        'GTE'
-    ]
+    relop_symbols = {
+        '==': '==',
+        '!=': '!=',
+        '<': '<',
+        '<=': '<=',
+        '>': '>',
+        '>=': '>='
+    }
 
-    logical_symbols = [
-        'and',
-        'or',
-    ]
+    logical_symbols = {
+        'and': '&&',
+        'or': '||',
+        'not': '!',
 
-    arithmetic_symbols = [
+        '&&': '&&',
+        '||': '||',
+        '!': '!',
+    }
+
+    operator_symbols = {
         # arithmetic
-        'PLUS',
-        'MINUS',
-        'TIMES',
-        'DIVIDE',
-        'POW',
-        'MOD'
-    ]
+        '+': '+',
+        '-': '-',
+        '*': '*',
+        '/': '/',
+        '//': '/',
+        '%': '%'
+    }
 
     # all the possible tokens
-    tokens = [
-        'ID',
-        'NUMBER',
-        'LOGIC',
-        'RELOP',
-        'ARITHMETIC',
-        'LPRAN',  # (
-        'RPRAN',  # )
-        'LBRACE',  # {
-        'RBRACE',  # }
-        'SEP',  # separator ,
-    ]
+    tokens = [ID, NUMBER, LOGIC, RELOP, OPERATOR, LPRAN, RPRAN, LBRACE, RBRACE, SEP, ]
     tokens += list(reserved.values())
+    # remove duplicate values
+    tokens = list(set(tokens))
 
     # simple tokens regex definition
     t_LPRAN = r'\('
@@ -60,6 +83,27 @@ class P2CLexer(object):
     t_LBRACE = r'\{'
     t_RBRACE = r'\}'
     t_SEP = r'\,'
+
+    def t_ID(self, t):
+        r"""[a-zA-Z_][a-zA-Z_0-9]*"""
+
+        # detect reserved word
+        if t.value in self.reserved.keys():
+            # detect logical reserved word
+            _token_mapped_value = self.reserved.get(t.value)
+            if _token_mapped_value == 'LOGIC':
+                t.type = 'LOGIC',
+                t.value = self.logical_symbols.get(t.value)
+
+            # not logical reserved word
+            else:
+                t.type = t.value
+                t.value = _token_mapped_value
+        return t
+
+    def t_NUMBER(self, t):
+        r"""\d+\.?\d+"""
+        t.type = 'NUMBER'
 
     # Define a rule so we can track line numbers
     def t_newline(self, t):
